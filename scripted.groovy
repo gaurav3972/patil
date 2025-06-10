8d186add7adf2d0e28e07c161a89c32504c04ac8
Gaurav Patil <3972gauravpatil@gmail.com>
2025-06-10 18:16:52 +0530

    add1

--- scripted.groovy
+++ scripted.groovy
@@ -1,14 +1,14 @@
-node {  
+node {
     stage('pull') {
-         git branch: 'main', url: 'git branch: 'main', url:'https://github.com/iamtruptimane/ECS-blue-green-deployment.git'
+        git branch: 'main', url: 'https://github.com/iamtruptimane/ECS-blue-green-deployment.git'
     }
     stage('Build') {
-         echo 'build is succeed'
+        echo 'Build succeeded'
     }
     stage('Test') {
-         echo 'Test cases going well'
+        echo 'Test cases passed'
     }
     stage('Deploy') {
-         echo 'yahoo!! application deployed successfully'
+        echo 'Yahoo!! Application deployed successfully'
     }
 }
